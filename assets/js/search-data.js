fcc16735b31224e448f94c01f1fa2fa96af7612e
Pulga2104 <josuestevenc@outlook.com>
2025-11-03 17:18:21 +0000

    Deploying to gh-pages from @ Pulga2104/CalvopinaJosue.github.io@6c4690eb71bae1f866c80e7cc5b54cddc47cb921 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/CalvopinaJosue.github.io/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/CalvopinaJosue.github.io/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
@@ -44,28 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/CalvopinaJosue.github.io/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/CalvopinaJosue.github.io/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/CalvopinaJosue.github.io/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/CalvopinaJosue.github.io/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
