2b80e880fd69f791221ea7cc16841d012e234b1d
Pulga2104 <Pulga2104@users.noreply.github.com>
2025-11-03 18:22:52 +0000

    deploy: 3b6dd79e7036e9513c0ad854dfaaec037fa1a259

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Pulga2104/CalvopinaJosue.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
